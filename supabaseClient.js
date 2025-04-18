2dde5013e5d1b9a74d0007c87bb4c8dea6efd266
Dozie130 <140114673+Dozie130@users.noreply.github.com>
2025-04-18 19:05:24 +0100

    supabaseClient.js

---
+++ supabaseClient.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = https://wftxomgovsnghtgkptdc.supabase.co
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmdHhvbWdvdnNuZ2h0Z2twdGRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5OTE1MzMsImV4cCI6MjA2MDU2NzUzM30.Az4sOAXf6001IkZj6TK_CvIy7gNnoI1GJciVqZcWzZQ';
+export const supabase = createClient('https://wftxomgovsnghtgkptdc.supabase.co'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmdHhvbWdvdnNuZ2h0Z2twdGRjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0NDk5MTUzMywiZXhwIjoyMDYwNTY3NTMzfQ.p7a0YhgIr2ltPYhvsQoxZhE8GMq8fM8N2LMs0pQH7wY);
